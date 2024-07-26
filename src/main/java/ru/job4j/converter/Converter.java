566ffefe0c27b7807c5162508ae7b86fb8eb469c
Jeka28882 <new28882@yandex.ru>
2024-07-26 21:54:54 +0300

    Тест

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -5,16 +5,27 @@ public class Converter {
         float result = value / 70;
         return result;
     }
-
-    public static float rubleToDollar(float value) {
-        float result = value / 60;
+    public static int rubleToDollar(int value) {
+        int result = value / 60;
         return result;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
+        float inEuro = 140;
+        float exEuro = 2;
+        float outputEuro = Converter.rubleToEuro(inEuro);
+        boolean passedEuro = exEuro == outputEuro;
+        System.out.println("140 rubles are " + euro + " euro. Test result : " + passedEuro);
+
+        float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar");
+        int inDollar = 140;
+        float exDollar = 2;
+        float outputDollar = Converter.rubleToDollar(inDollar);
+        boolean passedDollar = exDollar == outputDollar;
+        System.out.println("140 rubles are " + dollar + " euro. Test result : " + passedDollar);
+
     }
 }
