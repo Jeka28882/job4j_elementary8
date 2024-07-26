db04a223f7eed8a57b4e7000b1846af3b333d971
Jeka28882 <new28882@yandex.ru>
2024-07-26 20:39:29 +0300

    Идеальный вес

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double result = ((height - 100) * 1.15);
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = ((height - 110) * 1.15);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 170;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Man 170 is " + man);
+        System.out.println("Women 170 is " + woman);
+    }
+}
