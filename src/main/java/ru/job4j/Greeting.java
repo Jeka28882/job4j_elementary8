71373a6a52853a196f74ddf093bc6dccd4fc0ffa
Jeka28882 <new28882@yandex.ru>
2024-07-26 20:46:58 +0300

    Изучаю переменную стринг

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        int yar = 2014;
+        idea += yar;
+        System.out.println(idea);
+
+    }
+
+}
