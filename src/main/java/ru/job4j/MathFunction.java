861eee6fc3f07cf7247fbb3c08c7f1e65e49a960
Jeka28882 <new28882@yandex.ru>
2024-07-26 20:33:56 +0300

    Дописал код

---
+++ src/main/java/ru/job4j/MathFunction.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class MathFunction {
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static void main(String[] args) {
+
+        int result3 = MathFunction.func1(100);
+        System.out.println(result3);
+    }
+}
+
